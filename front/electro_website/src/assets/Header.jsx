6f9a0ed79add0a785c40f18fd9a2540519404842
akram <son.gogo@hotmail.com>
2024-10-15 17:55:01 +0100

    nav

--- front/electro_website/src/assets/Header.jsx
+++ front/electro_website/src/assets/Header.jsx
@@ -1,14 +1,24 @@
+import elect_logo from "./elect_logo.png";
 export default function Header() {
   return (
-    <nav className="grid grid-cols-[1fr_2fr] pl-10 mt-4 items-center">
-      <a href="">
-        <span className="text-3xl">Electro club</span>
+    <nav className="grid bg-NAV text-white   grid-cols-[1fr_3fr] pl-10 py-4 items-center">
+      <a href="" className="flex items-center gap-2">
+        <img src={elect_logo} className="size-10" alt="logo" />
+        <span className="text-3xl font-bold">Electro club</span>
       </a>
-      <div className="flex  justify-around">
-        <a href="">ABOUT US</a>
-        <a href="">ACTIVITIES</a>
-        <a href="">DISCORD</a>
-        <a href="">CONTACT</a>
+      <div className="flex font-semibold justify-around">
+        <a href="" className="hover:text-ELECT">
+          ABOUT US
+        </a>
+        <a href="" className="hover:text-ELECT">
+          ACTIVITIES
+        </a>
+        <a href="" className="hover:text-ELECT">
+          DISCORD
+        </a>
+        <a href="" className="hover:text-ELECT">
+          CONTACT
+        </a>
       </div>
     </nav>
   );
--- front/electro_website/tailwind.config.js
+++ front/electro_website/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ELECT: "#77C02E",
+        NAV: "#393939",
+        BG: "#212121",
+      },
+    },
   },
   plugins: [],
 };
