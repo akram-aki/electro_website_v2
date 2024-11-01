591c2f67a1f5c5c544bd7967f0de119a73c508a2
Youcef Boubidi <youcefboubidi3@gmail.com>
2024-11-01 15:39:25 +0100

    test simple server

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Use process.env.PORT for Azure
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Serve static files from the ./client/dist directory
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
-// Serve index.html for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.send("Welcome to the home page!");
 });
+
+app.get('/test', (req, res) => {
+  res.send("Server is working!");
+});
+
 
 // Start the server
 app.listen(PORT, () => {
