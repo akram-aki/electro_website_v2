4e37944ac7f426d4bebab34a5b600c6b445c9715
Youcef Boubidi <youcefboubidi3@gmail.com>
2024-11-01 14:50:06 +0100

    removed dist from git ignore

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import routes from "./components/routes.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+// Get __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
@@ -10,6 +16,14 @@ app.use(
   })
 );
 app.use(express.json());
+
+// Serve static files from the front/electro_website/dist directory
+app.use(express.static(path.join(__dirname, '../front/electro_website/dist')));
+
+// Serve the index.html file for the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front/electro_website/dist/index.html'));
+});
 
 app.use("/", routes);
 
