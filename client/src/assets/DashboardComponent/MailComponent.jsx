5df17403e6db12f1cf91f1447502a31b36f0d4e4
Youcef Boubidi <youcefboubidi3@gmail.com>
2024-11-01 15:48:39 +0100

    screw everything

--- client/src/assets/DashboardComponent/MailComponent.jsx
+++
@@ -1,5 +0,0 @@
-function MailComponent() {
-  return <div className="text-6xl text-white">Work in progress :p</div>;
-}
-
-export default MailComponent;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import express from 'express';
-
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000; // Use process.env.PORT for Azure
+
+// Serve static files from the ./client/dist directory
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+// Serve index.html for the root URL
 app.get('/', (req, res) => {
-  res.send("Welcome to the home page!");
+    res.send("Server is working!");
 });
-
-app.get('/test', (req, res) => {
-  res.send("Server is working!");
-});
-
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
